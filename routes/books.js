e0e85d8095e7e6b7e10344a3fde8f2fb19302cae
lluon <lluon001@gold.ac.uk>
2025-11-18 19:18:43 +0000

    v2 working

--- routes/books.js
+++ routes/books.js
@@ -1,39 +1,39 @@
-// Create a new router
-const { name } = require("ejs");
-const express = require("express")
-const router = express.Router()
+// routes/books.js
+const express = require("express");
+const router = express.Router();
 
+// search form
 router.get('/search',function(req, res, next){
     res.render("search.ejs")
 });
 
    //search results (partial match)
 router.get('/search-result', function (req, res, next) {
-    let keyword = req.query.keyword || ''
-    let sqlquery = "select from Books Where name like ?"
-    let searchTerm = '%${keyword}'
+    let keyword = req.query.keyword || "";
+    let sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+    let searchTerm = '%${keyword}%';
 
-    db.query(sqlquery,searchTerm, (err,result)=> {
-        if (err) {next(err)}
-        res.render("list.ejs",{availablebooks:result})
-    })
-})
+    db.query(sqlquery,[searchTerm], (err,result)=> {
+        if (err) return next(err);
+        res.render("list.ejs",{availableBooks:result})
+    });
+});
  
 // Full List
   router.get('/list', function(req, res, next) {
-    db.query("select from books", (err,result)=> {
-        if (err) {next(err)}
-        res.render("list.ejs",{availablebooks: result}) 
-    })
-  })
+    db.query("SELECT * FROM books", (err,result)=> {
+        if (err) return next(err);
+        res.render("list.ejs",{availableBooks: result});
+    });
+  });
 
 // Bargain books
   router.get('/bargainbooks', function(req, res, next) {
-    db.query("select from books", (err,result)=> {
-        if (err) {next(err)}
-        res.render("list.ejs",{availablebooks: result}) 
-    })
-  })
+    db.query("SELECT * FROM books WHERE price <20", (err,result)=> {
+        if (err) return next(err)
+        res.render("list.ejs",{availableBooks: result}) 
+    });
+  });
     
 // add book form
   router.get('/addbook', function(req, res, next) {
@@ -41,20 +41,19 @@ router.get('/search-result', function (req, res, next) {
     })
 
 // handle add book submission
-  router.get('/bookadded', function(req, res, next) {
-    let sqlquery = "insert into Books (name,price) values (?,?)"
-    let newrecord = [req.body.name,req.body.price]
+  router.post('/bookadded', function(req, res, next) {
+    let sqlquery = "INSERT INTO Books (name,price) values (?,?)"
+    let newrecord = [req.body.name,req.body.price];
     
     db.query(sqlquery,newrecord, (err,result)=> {
-        if (err) {next(err)}
-        else {
+        if (err) return next(err);
         res.render("bookadded.ejs",{
             name: req.body.name,
             price: req.body.price
-            })
-        }
-    })
-  })
+            });
+        });
+    });
+ 
   
 // Export the router object 
 module.exports = router
--- routes/main.js
+++ routes/main.js
@@ -1,22 +1,24 @@
-// Create a new router
+// routres/main.js
 const express = require("express")
 const router = express.Router()
 
 // Handle our routes
-router.get('/',function(req, res, next){
+router.get('/',(req, res, next) => {
     res.render('index.ejs')
 });
 
-router.get('/about',function(req, res, next){
+router.get('/about',(req, res, next) => {
     res.render('about.ejs')
 });
+
+
 // Registration success page - work from anywhere
-router.post('/registered'),function(req, res, next){
-    const first = req.body.first.trim()
-    const last = req.body.last.trim()
-    const email = req.body.email.trim()
-    res.send('Hello ${first} ${last}, you are noew registered! We will send an email to ${email}.')
-}
+router.post('/registered',(req, res, next) => {
+const first = req.body.first || '';
+    const last = req.body.last || '';
+    const email = req.body.email || '';
+    res.send('Hello ${first} ${last}, you are now registered! We will send an email to ${email}.')
+});
 
 // Export the router object so index.js can access it
 module.exports = router
