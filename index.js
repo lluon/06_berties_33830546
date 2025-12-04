83fb48e99045a87a61ea0dd1c29c1268cf8c32bc
lluon <lluon001@gold.ac.uk>
2025-12-04 12:03:05 +0000

    index 404 search page

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(BASE_PATH,express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressSanitizer());
 
 app.use(session({
@@ -43,13 +43,15 @@ app.locals.BASE_PATH = BASE_PATH;
 // API routes
 app.use('/api', apiRoutes);
 
-app.use(BASE_PATH + '/', mainRoutes);
-app.use(BASE_PATH + '/users', usersRoutes);
-app.use(BASE_PATH + '/books', booksRoutes);
-app.use(BASE_PATH + '/weather', weatherRoutes);
+app.use('/', mainRoutes);
+app.use('/users', usersRoutes);
+app.use('/books', booksRoutes);
+app.use('/weather', weatherRoutes);
+
 
 // 404 handler 
 app.use((req, res) => {
+  console.log('404 Debug - Original URL:', req.originalUrl);// check where is failing
   res.status(404).send(`
     <h1>404 Not Found</h1>
     <p>The requested URL ${req.originalUrl} was not found on this server.</p>
